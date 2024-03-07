3ea836bd7c20b86d25ebd9364fc0dd20a34ee7ae
Łukasz Kurant <36734207+lukaszkurantdev@users.noreply.github.com>
2024-03-07 13:06:34 +0100

    Update Segment.java

--- src/Segment.java
+++ src/Segment.java
@@ -26,4 +26,23 @@ public class Segment {
                 p1.y, p2.x, p2.y
         );
     }
+
+    public static Segment[] perpendicular(Segment line, Point point, double r) {
+        double a;
+        a = (line.p1.y - line.p2.y) / (line.p1.x - line.p2.x);
+        double b;
+        a=-1/a;
+        b=point.y-a*point.x;
+
+        double x0 = point.x;
+        double y0 = point.y;
+
+        double root = Math.sqrt(-y0*y0+(2*a*x0+2*b)*y0-a*a*x0*x0-2*a*b*x0+(a*a+1)*r*r-b*b);
+        double x1 = -(root-a*y0-x0+a*b)/(a*a+1);
+        double y1 = -(a*root-a*a*y0-a*x0-b)/(a*a+1);
+        double x2 = (root+a*y0+x0-a*b)/(a*a+1);
+        double y2 = (a*root+a*a*y0+a*x0+b)/(a*a+1);
+
+        return new Segment[]{new Segment(point, new Point(x1,y1)), new Segment(point, new Point(x2,y2))};
+    }
 }
