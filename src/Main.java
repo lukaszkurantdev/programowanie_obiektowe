e91b6a4e1a119fda91ac3330d889a926f4867cf7
lukaszkurantdev <contact@lukaszkurant.com>
2024-03-21 13:28:05 +0100

    post lab3

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,6 @@
 
-
-//https://github.com/lukaszkurantdev/programowanie_obiektowe
-
 public class Main {
     public static void main(String[] args) {
-        Vec2 point1 = new Vec2(0, 100);
-        Vec2 point2 = new Vec2(100, 102);
-        Vec2 point3 = new Vec2(50, 50);
 
-        Polygon polygon = new Polygon(
-                3,
-                new Style(null, null, null)
-        );
-        polygon.setPoint(0, point1);
-        polygon.setPoint(1, point2);
-        polygon.setPoint(2, point3);
-
-        Shape filledPolygon = new SolidFilledShapeDecorator(polygon, "blue");
-
-        Shape transformedPolygon = new TransformationDecorator
-                .Builder()
-                .setShape(filledPolygon)
-                .setTranslateVector(point3)
-                .setTranslateVector(point3)
-                .build();
-
-        SvgScene svgScene = new SvgScene();
-        svgScene.addShape(transformedPolygon);
-        svgScene.saveHtml("./test.html");
     }
 }
--- src/Vec2.java
+++
@@ -1,9 +0,0 @@
-public class Vec2 {
-    public  double x;
-    public final double y;
-
-    public Vec2(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-}
