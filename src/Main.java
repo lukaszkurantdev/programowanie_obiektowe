29df04a69f55d753c1c4e2d6822e3885c0aa01f4
Łukasz Kurant <lukaslux@outlook.com>
2024-04-04 15:46:20 +0200

    Lab5 z1

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,27 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            List<Person> people = Person.fromCsv("family.csv");
-            for(Person person: people)
-                System.out.println(person);
+        String data = "@startuml\n" +
+                "object JanKowalski\n" +
+                "object AnnaKowalska\n" +
+                "\n" +
+                "JanKowalski --> AnnaKowalska\n" +
+                "@enduml";
 
-            Person.toBinaryFile(people, "family.bin");
-            List<Person> loadPeople = Person.fromBinaryFile("family.bin");
-            for(Person person: loadPeople)
-                System.out.println(person);
-
-        } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        PlantUMLRunner.setJarPath("plantuml-1.2024.3.jar");
+        PlantUMLRunner.generate(data, "uml", "test");
+//        try {
+//            List<Person> people = Person.fromCsv("family.csv");
+//            for(Person person: people)
+//                System.out.println(person);
+//
+//            Person.toBinaryFile(people, "family.bin");
+//            List<Person> loadPeople = Person.fromBinaryFile("family.bin");
+//            for(Person person: loadPeople)
+//                System.out.println(person);
+//
+//        } catch (IOException | ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
