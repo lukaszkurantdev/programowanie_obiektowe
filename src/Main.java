d8f2c840f775191cedd3e8087c37a6c742066440
Łukasz Kurant <lukaslux@outlook.com>
2024-04-04 16:53:03 +0200

    Lab5 pre z4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,11 +10,24 @@ public class Main {
 
         try {
             List<Person> people = Person.fromCsv("family.csv");
-            for(Person person: people)
-                System.out.println(person);
 
-            String uml = Person.toUML(people);
-            PlantUMLRunner.generate(uml, "uml", "family");
+//            Optional<String> changedPeople = people
+//                    .stream()
+//                    .sorted((person1, person2) -> person1.name.compareTo(person2.name))
+//                    .map(person -> person.name)
+//                    .filter(name -> !name.equals("Anna Dąbrowska"))
+//                    .max((name1, name2) -> name1.compareTo(name2));
+//                    .collect(Collectors.toList());
+
+//            if(!changedPeople.isEmpty()) {
+//                System.out.println(changedPeople.get());
+//            }
+
+//            for(String person: changedPeople)
+//                System.out.println(person);
+//
+//            String uml = Person.toUML(people);
+//            PlantUMLRunner.generate(uml, "uml", "family");
 
 //            Person person = people.get(2);
 //            String uml = person.toUML();
