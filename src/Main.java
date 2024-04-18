b0339e7a5e7b811804e1c5a4a4e6b3b6c81cab87
lukaszkurantdev <contact@lukaszkurant.com>
2024-04-18 17:19:42 +0200

    z2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-    String s = "12";
-        Integer.parseInt(s);
+        String path = "zgony.csv";
 
+        try {
+
+           List<DeathCauseStatistics> list = Files.lines(Paths.get(path))
+                    .skip(2)
+                    .map(v -> DeathCauseStatistics.fromCsvLine(v))
+                    .collect(Collectors.toList());
+"test".split(" ", 2)
+           int age = 32;
+           List<DeathCauseStatistics> result = list.stream().sorted(
+                   (x,y) -> Integer.compare(
+                           y.bracketDeaths(age).deathCount,
+                           x.bracketDeaths(age).deathCount
+                   )
+           ).collect(Collectors.toList()).subList(0,10);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
