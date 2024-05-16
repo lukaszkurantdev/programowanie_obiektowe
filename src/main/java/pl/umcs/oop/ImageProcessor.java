8c2cf901fab6368e91f84795698a2641f61a4d78
student <test@tets.pl>
2024-05-16 15:54:53 +0200

    z2

--- src/main/java/pl/umcs/oop/ImageProcessor.java
+++ src/main/java/pl/umcs/oop/ImageProcessor.java
@@ -21,4 +21,34 @@ public class ImageProcessor {
         File file = new File(path);
         ImageIO.write(image, "png", file);
     }
+
+    public void increaseBrightness(int factor) {
+        for(int x = 0 ; x < image.getHeight() ; x++) {
+            for(int y = 0 ; y < image.getWidth() ; y++) {
+                int pixel = image.getRGB(x, y);
+                pixel = brightenPixel(pixel, factor);
+                image.setRGB(x, y, pixel);
+            }
+        }
+    }
+
+    private int brightenPixel(int pixel, int factor) {
+        int mask = 255;
+        int blue = pixel & mask;
+        int green = (pixel >> 8) & mask;
+        int red = (pixel >> 16) & mask;
+        blue = brightenPixelPart(blue, factor);
+        green = brightenPixelPart(green, factor);
+        red = brightenPixelPart(red, factor);
+        return blue + (green << 8) + (red << 16);
+    }
+
+    private int brightenPixelPart(int color, int factor) {
+        color += factor;
+        if(color > 255) {
+            return 255;
+        } else {
+            return color;
+        }
+    }
 }
--- src/main/java/pl/umcs/oop/Main.java
+++ src/main/java/pl/umcs/oop/Main.java
@@ -1,7 +1,16 @@
 package pl.umcs.oop;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-
+        ImageProcessor ip = new ImageProcessor();
+        try {
+            ip.loadImage("image.png");
+            ip.increaseBrightness(100);
+            ip.saveImage("image2.png");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
