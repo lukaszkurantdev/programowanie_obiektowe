b4e442a114e438cfbde03620cb232dd82455136c
student <test@tets.pl>
2024-05-23 16:33:48 +0200

    z3

--- demo/src/main/java/com/example/demo/RectangleController.java
+++ demo/src/main/java/com/example/demo/RectangleController.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class RectangleController {
+    List<Rectangle> rectangles = new ArrayList<>();
+
     @GetMapping("rectangle")
     public Rectangle getRectangle() {
         Rectangle rectangle = new Rectangle(
@@ -12,5 +18,22 @@ public class RectangleController {
                 100,213,
                 "red");
         return rectangle;
+    }
+
+    @PostMapping("addRectangle")
+    public int addRectangle() {
+        Rectangle rectangle = new Rectangle(
+                20,10,
+                100,213,
+                "red");
+
+        rectangles.add(rectangle);
+
+        return rectangles.size();
+    }
+
+    @GetMapping("rectangles")
+    public List<Rectangle> getRectangles() {
+        return rectangles;
     }
 }
