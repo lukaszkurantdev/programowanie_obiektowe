2cb398b6511abff00d9108261dc93e0ac211d6b0
student <test@tets.pl>
2024-06-13 16:08:13 +0200

    z2

--- client_gui/src/main/java/org/example/client_gui/HelloApplication.java
+++ client_gui/src/main/java/org/example/client_gui/HelloApplication.java
@@ -3,18 +3,29 @@ package org.example.client_gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TextInputDialog;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Logowanie");
+        dialog.setContentText("Wpisz login");
+
+        Optional<String> login = dialog.showAndWait();
+
+        if(login.isPresent()) {
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
     public static void main(String[] args) {
