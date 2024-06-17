2a229ecd7b7b5f906805fe35c5e28a28eff03072
student <test@tets.pl>
2024-06-17 16:16:02 +0200

    z3

--- k2_client/src/main/java/org/example/k2_client/HelloController.java
+++ k2_client/src/main/java/org/example/k2_client/HelloController.java
@@ -1,8 +1,11 @@
 package org.example.k2_client;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -10,6 +13,8 @@ import java.util.List;
 
 public class HelloController {
     public Label wordCountLabel;
+    public TextField filterField;
+    public ListView wordList;
 
     public HelloController() {
         ClientReceiver.controller = this;
@@ -20,6 +25,17 @@ public class HelloController {
     public void onWordReceived(String word) {
         listOfWords.add(new Word(word, LocalTime.now()));
 
-        Platform.runLater(()->{wordCountLabel.setText(String.valueOf(listOfWords.size()));});
+        Platform.runLater(()->{
+            wordCountLabel.setText(String.valueOf(listOfWords.size()));
+            update();
+        });
+    }
+
+    public void update() {
+        wordList.setItems(FXCollections.observableArrayList(
+                listOfWords.stream()
+                        .map((item) -> item.string)
+                        .toList()
+        ));
     }
 }
