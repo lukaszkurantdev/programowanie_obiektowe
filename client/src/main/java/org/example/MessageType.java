a73bc6797f14ce11a5268cadb27b0dad4a2c5870
student <test@tets.pl>
2024-06-17 13:53:42 +0200

    pre lab 14

--- client/src/main/java/org/example/MessageType.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public enum MessageType {
-    Broadcast,
-    Login
-}
--- client_gui/src/main/java/org/example/client_gui/MessageType.java
+++
@@ -1,6 +0,0 @@
-package org.example.client_gui;
-
-public enum MessageType {
-    Broadcast,
-    Login
-}
